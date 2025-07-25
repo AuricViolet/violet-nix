bef53b8aea184a66317f3030b755eb88d6a1441e
AuricViolet <matthew.k.1156@gmail.com>
2025-07-25 09:13:25 -0300

    Delete sddmdisplaysetup.sh

--- sddmdisplaysetup.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-export DISPLAY=:0
-sleep 2
-
-xrandr --output DVI-D-1 --mode 1920x1080 --rate 143.88 --pos 0x564 --primary
-xrandr --output DP-2 --mode 1920x1080 --rate 59.96 --pos 1920x0 --rotate left
