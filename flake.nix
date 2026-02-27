1ecb27a7d2fd74728267e7d5d89cd2e4abe26702
AuricViolet <matthew.k.1156@gmail.com>
2026-02-26 23:08:19 -0400

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Nixos config flake";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     spicetify-nix.url = "github:Gerg-L/spicetify-nix";
     nvf = {
       url = "github:notashelf/nvf";
